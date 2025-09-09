2631fc9afc26502ca266a4fe90e2f246adfe7dbc
JohnCarl1056 <jcgonzalez0803@yahoo.com>
2025-09-09 11:55:49 -0700

    Add files via upload

---
+++ ArrayPractice.java
@@ -0,0 +1,21 @@
+// ArrayPractice.java
+// jg 9/9/25
+public class ArrayPractice {
+    public static void main(String[] args) {
+
+        System.out.println("Array Practice");
+
+        String[] myFourStrings = new String[4];
+        myFourStrings[0] = "one";
+        myFourStrings[1] = "two";
+        myFourStrings[2] = "three";
+        myFourStrings[3] = "four";
+
+        for (int i=0; i>=; i++) {
+            System.out.println(myFourStrings[i]);
+        }
+
+
+
+    }
+}
